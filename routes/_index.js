ff2fa5c9824e3acd175bf812c8946ac2c8ee6308
garaemon <garaemon@gmail.com>
2014-03-13 23:10:28 +0900

    fix typo

--- routes/_index.js
+++ routes/_index.js
@@ -3,5 +3,5 @@ var CONFIG = require('../config');
 exports.get_url = '/';
 
 exports.get = function(req, res){
-  res.render('index', { title: CONFIG.TITLE });
+  res.render('index', { title: CONFIG.title });
 };
